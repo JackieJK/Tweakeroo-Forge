c0bc32aac7df6a74f5db1efba889edbc54e99baa
TexTrue <3140846162@qq.com>
2024-05-29 19:25:33 +0800

    update MaFgLib version

--- src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
+++ src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
@@ -1,36 +1,11 @@
 package fi.dy.masa.tweakeroo;
 
-import fi.dy.masa.malilib.compat.forge.ForgePlatformUtils;
-import fi.dy.masa.tweakeroo.gui.GuiConfigs;
-import fi.dy.masa.tweakeroo.tweaks.PlacementTweaks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import fi.dy.masa.malilib.event.InitializationHandler;
 
-@Mod(Reference.MOD_ID)
 public class Tweakeroo {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
     public static int renderCountItems;
     public static int renderCountXPOrbs;
-
-    public Tweakeroo() {
-        if (FMLLoader.getDist().isClient()) {
-            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
-            InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-            ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
-                GuiConfigs gui = new GuiConfigs();
-                gui.setParent(screen);
-                return gui;
-            });
-
-            MinecraftForge.EVENT_BUS.<PlayerDestroyItemEvent>addListener(event -> {
-                PlacementTweaks.onProcessRightClickPost(event.getEntity(), event.getHand());
-            });
-        }
-    }
 }
