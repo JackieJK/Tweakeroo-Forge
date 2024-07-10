3cd0e78eab39bad5dbcd39c68b8d0b499d6c22e9
TexTrue <3140846162@qq.com>
2024-07-10 18:12:40 +0800

    sync with `sakura-ryoko/tweakeroo`

--- src/main/java/org/thinkingstudio/tweakerge/Tweakerge.java
+++ src/main/java/org/thinkingstudio/tweakerge/Tweakerge.java
@@ -6,14 +6,15 @@ import fi.dy.masa.tweakeroo.gui.GuiConfigs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
-import org.thinkingstudio.mafglib.util.ForgePlatformUtils;
+import org.thinkingstudio.mafglib.util.NeoUtils;
 
 @Mod(value = Reference.MOD_ID, dist = Dist.CLIENT)
 public class Tweakerge {
     public Tweakerge() {
         if (FMLLoader.getDist().isClient()) {
             Tweakeroo.onInitialize();
-            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
+
+            NeoUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
                 GuiConfigs gui = new GuiConfigs();
                 gui.setParent(screen);
                 return gui;
